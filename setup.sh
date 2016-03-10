cf1af5ac0cd0516b27f544971997e04da3be6a43
Michael Goodwin <xenithorb@users.noreply.github.com>
2016-03-10 06:08:29 -0500

    Add .vim folder and hybrid vim theme
    Replaced hybrid symlink with actual theme
    Modified setup.sh to remove existing home dotfile directories

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+find $PWD -mindepth 1 -maxdepth 1 -type d -print0 | \
+	while read -rd '' dir; do
+		echo rm -rf ~/"${dir##*/}"
+	done
+
 find $PWD -mindepth 1 -maxdepth 1 ! -name ".git" -name ".*" -exec ln -svf '{}' ~/ \;
+
 symlinks -c ~/
