8103e1529e54532f99f393e7c16255494e8c5766
Michael Goodwin <xenithorb@users.noreply.github.com>
2016-04-27 15:49:01 -0400

    setup.sh: shellcheck quoting error fix
    Other: whitespace fixes

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-find $PWD -mindepth 1 -maxdepth 1 -type d ! -name ".git" -print0 | \
+find "$PWD" -mindepth 1 -maxdepth 1 -type d ! -name ".git" -print0 | \
 	while read -rd '' dir; do
 		echo rm -rf ~/"${dir##*/}"
 	done
 
-find $PWD -mindepth 1 -maxdepth 1 ! -name ".git" -name ".*" -exec ln -svf '{}' ~/ \;
+find "$PWD" -mindepth 1 -maxdepth 1 ! -name ".git" -name ".*" -exec ln -svf '{}' ~/ \;
 
 symlinks -c ~/
