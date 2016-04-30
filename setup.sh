b19e35fb534f61b0cf88ce66d5caff64737a4adc
Michael Goodwin <xenithorb@users.noreply.github.com>
2016-04-30 01:11:03 -0400

    setup.sh: Check for `symlinks`

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+hash symlinks 2>/dev/null 2>&1 || { echo 'Go install symlinks!'; exit 1; }
+
 find "$PWD" -mindepth 1 -maxdepth 1 -type d ! -name ".git" -print0 | \
 	while read -rd '' dir; do
 		echo rm -rf ~/"${dir##*/}"
