1819748d9d2ee3ec3d2b5631cebb27cc5d8dd4ad
Michael Goodwin <xenithorb@users.noreply.github.com>
2016-05-13 17:54:49 -0400

    setup.sh: safe improvements with action output

--- setup.sh
+++ setup.sh
@@ -1,12 +1,42 @@
 #!/bin/bash
-
+#
+# Setup script for 'dotfiles' repo.
+# will create symlinks for files in the repo in ~/
+#
+#
+# Needs symlinks to tidy up absolute positions
 hash symlinks 2>/dev/null 2>&1 || { echo 'Go install symlinks!'; exit 1; }
 
-find "$PWD" -mindepth 1 -maxdepth 1 -type d ! -name ".git" -print0 | \
-	while read -rd '' dir; do
-		echo rm -rf ~/"${dir##*/}"
-	done
+# Comment this out or set 0 you don't want it to overwrite
+# or remove the necessary files/directories
+OVERWRITE=1
 
-find "$PWD" -mindepth 1 -maxdepth 1 ! -name ".git" -name ".*" -exec ln -svf '{}' ~/ \;
+if [[ "$OVERWRITE" == 1 ]]; then
+	rm_Owrite="-f"
+	ln_Owrite="-f"
+fi
 
+RDIR="$(dirname "$0")"
+ADIR="$(readlink -m "$RDIR")"
+
+print_half_width_line() {
+	print_line_with_name() {
+		printf "\n%s\n%s\n%s\n" "${line}" "${1}" "${line}"
+	}
+	if [[ $line ]]; then
+		print_line_with_name "$1"
+	else
+		line="$(eval printf '%.s-' {1..$(( $(tput cols)/2 ))})"
+		print_line_with_name "$1"
+	fi
+}
+
+
+print_half_width_line "Directories:"
+find "$RDIR" -mindepth 1 -maxdepth 1 -type d ! -name ".git" -exec sh -c 'rm '"$rm_Owrite"' -rv '"$HOME/"'$(basename {})' \;
+print_half_width_line "Files:"
+find "$RDIR" -mindepth 1 -maxdepth 1 ! -name ".git" -name ".*" -exec ln -sv "$ln_Owrite" "$ADIR/{}" "$HOME/" \;
+print_half_width_line "END"
+
+# Convert absolute links to relative links the right way
 symlinks -c ~/
